2cd8d5c090b5f03f101b1ec9a052578405e25bff
khanhpro1250 <51900114@student.tdtu.edu.vn>
2021-10-24 21:47:37 +0700

    connect mongodb

--- server/server.js
+++ server/server.js
@@ -1,13 +1,13 @@
 const mongoose = require('mongoose')
 
 async function connect() {
-    // try {
-    //     await mongoose.connect('mongodb://localhost:27017/advanced-web-lab3')
-    //     console.log('connect successfully')
-    // }
-    // catch (err) {
-    //     console.log("connection failed")
-    // }
+    try {
+        await mongoose.connect(process.env.mongodb)
+        console.log('connect successfully')
+    }
+    catch (err) {
+        console.log("connection failed")
+    }
 }
 
 module.exports = { connect }
