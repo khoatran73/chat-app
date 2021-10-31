71dcd5fbde9f8e0f7ed9e13f2c50cd667e93dfef
KhoaHenry <trananhkhoa73@gmail.com>
2021-10-31 16:01:54 +0700

    fix bug in client.js

--- public/js/client.js
+++ public/js/client.js
@@ -1,60 +1,64 @@
 
 
-(function(){
-    let chatBox = document.querySelector("#chat-box") 
-    chatBox.scrollTop = chatBox.scrollHeight
-    const chatform= $("#chat-form");
-    const chatMsg= $('#message');
+(function () {
+    let chatBox = document.querySelector("#chat-box")
+
+    if (chatBox) {
+        chatBox.scrollTop = chatBox.scrollHeight
+    }
+
+    const chatform = $("#chat-form");
+    const chatMsg = $('#message');
     let ws;
-    function show_receiveMessage(message){
+    function show_receiveMessage(message) {
         $('#chat-box').append(
-        `<div class="chat guest">
+            `<div class="chat guest">
             <div class="details">
                 <p>${message}</p>
             </div>
         </div>`)
         chatBox.scrollTop = chatBox.scrollHeight
     }
-    function show_sendMessage(message){
+    function show_sendMessage(message) {
         $('#chat-box').append(
-        `<div class="chat host">
+            `<div class="chat host">
             <div class="details">
                 <p>${message}</p>
             </div>
         </div>`)
         chatBox.scrollTop = chatBox.scrollHeight
     }
-    function init(){
-        
-        if(ws){
-            ws.onerror=ws.onopen=ws.onclose=null;
+    function init() {
+
+        if (ws) {
+            ws.onerror = ws.onopen = ws.onclose = null;
             ws.close();
         }
 
-        const server_url=`ws://localhost:3000`
-        ws= new WebSocket(server_url)
-        ws.onopen=()=>{
+        const server_url = `ws://localhost:3000`
+        ws = new WebSocket(server_url)
+        ws.onopen = () => {
             console.log('Connecting to server')
         }
-        ws.onmessage=({data})=>{
+        ws.onmessage = ({ data }) => {
             show_receiveMessage(data)
         }
-        ws.onclose=function(){
-            ws=null;
+        ws.onclose = function () {
+            ws = null;
         }
-        chatform.submit(e =>{
+        chatform.submit(e => {
             e.preventDefault();
-            if(!ws){
+            if (!ws) {
                 show_sendMessage('No websocket connection')
-                return ;
+                return;
             }
             ws.send(JSON.stringify(chatMsg.val()))
             show_sendMessage(chatMsg.val())
             chatMsg.val("");
             chatMsg.focus();
-            
+
         })
-        
+
     }
     init();
 
